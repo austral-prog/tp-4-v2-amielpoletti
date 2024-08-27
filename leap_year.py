9ab994f41457048402d4c2347247878953303eff
amielpoletti <162723122+amielpoletti@users.noreply.github.com>
2024-08-27 16:36:11 -0300

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,2 +1,12 @@
 def leap_year():
-    print("TO DO")
+    año= int(input("Ingrese un año: "))
+    
+    if año >= 0:
+      if año % 4 == 0 and año % 100 !=0:
+        print(f"El año {año} es bisiesto")
+      elif año % 100 == 0 and año % 400 == 0:
+            print(f"El año {año} es bisiesto")
+      else: 
+          print(f"El año {año} no es bisiesto")
+    else:
+      print("El año ingresado es incorrecto")
